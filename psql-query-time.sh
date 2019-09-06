73d37c8bfccdfc3ec63c0bf771c08a04fc41cf3c
Casey Litton <caseylitton@gmail.com>
2019-09-06 13:47:17 -0700

    [REVT] Add psql query script

---
+++ psql-query-time.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+query='SELECT keys.name AS keys_name, keys.value AS keys_value, keys.rid AS keys_rid, propsheets_1.sid AS propsheets_1_sid, propsheets_1.rid AS propsheets_1_rid, propsheets_1.name AS propsheets_1_name, propsheets_1.properties AS propsheets_1_properties, propsheets_1.tid AS propsheets_1_tid, current_propsheets_1.rid AS current_propsheets_1_rid, current_propsheets_1.name AS current_propsheets_1_name, current_propsheets_1.sid AS current_propsheets_1_sid, resources_1.rid AS resources_1_rid, resources_1.item_type AS resources_1_item_type FROM keys JOIN resources AS resources_1 ON resources_1.rid = keys.rid JOIN current_propsheets AS current_propsheets_1 ON resources_1.rid = current_propsheets_1.rid JOIN propsheets AS propsheets_1 ON current_propsheets_1.sid = propsheets_1.sid limit 1;'
+cd /srv/encoded
+sudo su - encoded psql -c $query
